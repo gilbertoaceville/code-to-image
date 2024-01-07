0470dbebca42ec75b536b3d2df4b785e0571ec54
gilbertoaceville <gilbertoaceville@gmail.com>
2024-01-07 08:11:50 +0100

    feat: setup project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
 
-module.exports = nextConfig
+const nextConfig = {};
+
+module.exports = (phase, { defaultConfig }) => {
+  if ("sassOptions" in defaultConfig)
+    defaultConfig["sassOptions"] = {
+      includePaths: [path.join(__dirname, "app/styles")],
+      prependData: `@import "app/styles/_base.scss";`,
+    };
+  const config = {
+    ...defaultConfig,
+    ...nextConfig,
+  };
+
+  return config;
+};
